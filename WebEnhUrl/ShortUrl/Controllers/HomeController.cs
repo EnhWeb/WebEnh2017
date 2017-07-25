db3f56232f6f66a6f04ca564c5247055bb6dc4f2
闵文辉 <101060493@qq.com>
2017-07-25 11:26:03 +0800

    init 短链接

---
+++ WebEnhUrl/ShortUrl/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShortUrl.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+
+            return Content($"id:{id}", "text/html", System.Text.Encoding.UTF8);
+
+            //return Redirect($"http://{id}");
+
+            //return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
