bc7b8c3bde49fd665afd4a15e629c7ce106a1316
闵文辉-笔记本 <101060493@qq.com>
2017-08-02 01:12:00 +0800

    富文本编器测试

---
+++ UEditorTest/UEditorTest/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UEditorTest
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add(new UEditor.UEditorRoute("/ueditors/Demo1/api", UEditorConfigure.Demo1));
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ UEditorTest/UEditorTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UEditorTest.MvcApplication" Language="C#" %>
