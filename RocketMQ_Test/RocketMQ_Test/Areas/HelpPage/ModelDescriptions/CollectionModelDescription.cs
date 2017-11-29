02e2311198faadf1e8bfaca03473dfb1f0754d7c
闵文辉-笔记本 <101060493@qq.com>
2017-11-29 23:24:33 +0800

    update

---
+++ RocketMQ_Test/RocketMQ_Test/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace RocketMQ_Test.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ RocketMQ_Test/RocketMQ_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RocketMQ_Test.WebApiApplication" Language="C#" %>
